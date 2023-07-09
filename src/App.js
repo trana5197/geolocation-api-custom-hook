4d7bdedd9f5c6b44bfc72fafd1db54b8a5d05c85
rana <trana51997@gmail.com>
2023-07-09 18:04:39 -0500

    Custom hook API for geolocation

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useGeolocation } from "./useGeolocation";
+
+export default function App() {
+  const [countClicks, setCountClicks] = useState(0);
+
+  const {
+    position: { lat, lng },
+    isLoading,
+    error,
+    getPosition,
+  } = useGeolocation();
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div className="app">
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
